b7a51a9dc5e2a5f44267b7ff2d6e47227967c5f6
matti <traceur775@gmail.com>
2017-04-09 13:17:56 -0700

    getting data from log file during frontend app init

--- stooq-reader/src/main/java/com/mbadziong/stooq/stooq/report/ReportWriter.java
+++ stooq-reader/src/main/java/com/mbadziong/stooq/stooq/report/ReportWriter.java
@@ -1,5 +1,7 @@
 package com.mbadziong.stooq.stooq.report;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mbadziong.stooq.stooq.data.model.MarketIndex;
 import com.mbadziong.stooq.stooq.report.model.ReportRow;
 import org.slf4j.Logger;
@@ -8,10 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,19 +27,48 @@ public class ReportWriter {
     @Value("${report_path}")
     private String reportPath;
 
+    private Gson gson;
+
+    public ReportWriter(Gson gson) {
+        this.gson = gson;
+    }
+
     public void writeToFile(MarketIndex marketIndex) {
         ReportRow row = new ReportRow(marketIndex);
-        List<String> lines = Arrays.asList(row.toString());
+        List<ReportRow> currentRows = getCurrentReport();
+        currentRows.add(row);
+        List<String> lines = Arrays.asList(gson.toJson(currentRows));
+
         try {
             Files.write(
                     Paths.get(reportPath),
                     lines,
                     Charset.defaultCharset(),
-                    StandardOpenOption.APPEND,
+                    StandardOpenOption.WRITE,
                     StandardOpenOption.CREATE
             );
         } catch (IOException e) {
             LOGGER.error("Error while appending new row to file.", e.getMessage());
         }
+    }
+
+    private List<ReportRow> getCurrentReport() {
+        List<String> content = new ArrayList<>();
+        try {
+            content = Files.readAllLines(Paths.get(reportPath));
+        } catch (IOException e) {
+            LOGGER.error("Error while reading file.");
+        }
+
+        String json = String.join("", content);
+        Type listType = new TypeToken<ArrayList<ReportRow>>() {
+        }.getType();
+        List<ReportRow> currentRows = gson.fromJson(json, listType);
+
+        if (currentRows == null) {
+            currentRows = new ArrayList<>();
+        }
+
+        return currentRows;
     }
 }
--- stooq-reader/src/main/resources/static/app/controllers/stooqHomeController.js
+++ stooq-reader/src/main/resources/static/app/controllers/stooqHomeController.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function StooqHomeController($rootScope, $scope, stooqData, stooqPlot) {
+    function StooqHomeController($rootScope, $scope, $http, stooqData, stooqPlot) {
 
         $scope.ctx = document.getElementById("canvas").getContext("2d");
 
@@ -12,19 +12,34 @@
             options: stooqPlot.defaultPlotOptions()
         });
 
-        $rootScope.$on('stooq-data-changed', function (event, stooqData) {
+        $http({
+            method: 'GET',
+            url: '/report.json'
+        }).then(function successCallback(response) {
+            var stooqDataRows = response.data;
+            for (var rowNum = 0; rowNum < stooqDataRows.length; rowNum++) {
+                $scope.loadNewStooqDataRow(stooqDataRows[rowNum].marketIndex);
+            }
+        }, function errorCallback(response) {
+            //nothing to init
+        });
+
+        $scope.loadNewStooqDataRow = function (stooqData) {
             $scope.lineChartData.labels.push(new Date());
             $scope.lineChartData.datasets[0].data.push(stooqData.wig);
             $scope.lineChartData.datasets[1].data.push(stooqData.wig20);
             $scope.lineChartData.datasets[2].data.push(stooqData.wig20fut);
             $scope.lineChartData.datasets[3].data.push(stooqData.mwig40);
             $scope.lineChartData.datasets[4].data.push(stooqData.swig80);
+        };
 
+        $rootScope.$on('stooq-data-changed', function (event, stooqData) {
+            $scope.loadNewStooqDataRow(stooqData);
             $scope.myLine.update();
         });
     }
 
     angular
         .module('stooqReader')
-        .controller('stooqHomeController', ['$rootScope', '$scope', 'StooqData', 'StooqPlot', StooqHomeController]);
+        .controller('stooqHomeController', ['$rootScope', '$scope', '$http', 'StooqData', 'StooqPlot', StooqHomeController]);
 }());
